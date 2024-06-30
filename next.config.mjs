bddeedcd7e437ec4f0d36d160902c72a07621ef4
Artur Campos <arturcamposba99@gmail.com>
2024-06-30 18:17:39 -0300

    feat: add cors

--- next.config.mjs
+++ next.config.mjs
@@ -9,21 +9,21 @@ const nextConfig = {
         port: "",
       },
     ],
-    async headers() {
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, OPTIONS",
-            },
-            { key: "Access-Control-Allow-Headers", value: "*" },
-          ],
-        },
-      ];
-    },
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
+      },
+    ];
   },
 };
 
